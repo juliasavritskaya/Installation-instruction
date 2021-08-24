68a9893684cadff5652a1a874ed678365dab31ca
juliasavritskaya <55387479+juliasavritskaya@users.noreply.github.com>
2021-08-24 14:20:39 +0300

    Update Task1.java

--- by/savritskaya/task1/Task1.java
+++ by/savritskaya/task1/Task1.java
@@ -2,7 +2,7 @@ package by.savritskaya.task1;
 
 public class Task1 {
 	public static void main (String[] args) {
-		String s = "Помогите пожалусто я думала разберусь с этими джарниками за 25 минут (НЕТ)";
+		String s = "Hello world";
 		System.out.println("Hello there");
 	}
 }
