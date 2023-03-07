ee4778a30e861c22a470a31dbbfd530e054fc66e
Julia Savritskaya <55387479+juliasavritskaya@users.noreply.github.com>
2023-03-08 02:55:54 +0400

    Change hardcored text to a variable

--- by/savritskaya/task1/Task1.java
+++ by/savritskaya/task1/Task1.java
@@ -3,6 +3,6 @@ package by.savritskaya.task1;
 public class Task1 {
 	public static void main (String[] args) {
 		String s = "Hello world";
-		System.out.println("Hello there");
+		System.out.println(s);
 	}
 }
